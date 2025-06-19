2f8571f8d7622ec88c6ed189848b3fea1a91975c
Igor Belevtsov <id.daenure@gmail.com>
2025-06-20 02:52:43 +0300

    task_2: Update: private subnet configuration

--- task_2/project/networking.tf
+++ task_2/project/networking.tf
@@ -14,10 +14,11 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count             = length(var.private_subnet_cidrs)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = element(var.azs, count.index)
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = element(var.azs, count.index)
+  map_public_ip_on_launch = false
   tags = {
     Name = "${var.project_name}-private-${var.environment_name}-${count.index + 1}"
   }
