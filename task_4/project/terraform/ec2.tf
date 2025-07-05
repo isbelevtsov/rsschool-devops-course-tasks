bb594881718a0e6240ffded266429e6f9fdf1b4c
Igor Belevtsov <id.daenure@gmail.com>
2025-07-05 15:49:21 +0300

    fix: remove unnecessary sudo from AWS SSM put-parameter command in K3s control plane provisioning

--- task_4/project/terraform/ec2.tf
+++ task_4/project/terraform/ec2.tf
@@ -198,7 +198,6 @@ resource "null_resource" "provision_k3s_control_plane" {
       "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--write-kubeconfig-mode 644' sh -",
       # "sudo curl -s http://169.254.169.254/latest/meta-data/local-ipv4 > /var/lib/rancher/k3s/server/ip",
       "aws ssm put-parameter --name \"${var.kubeconfig_param_path}\" --value file:///etc/rancher/k3s/k3s.yaml --type SecureString --overwrite",
-      "AWS_DEFAULT_REGION=\"${var.aws_region}\" sudo aws ssm put-parameter --name \"${var.node_token_param_path}\" --value file:///var/lib/rancher/k3s/server/node-token --type SecureString --overwrite"
     ]
   }
 }
